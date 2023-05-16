356c251bf463a234207d12823cbb862352ffb0be
eeroleppalehto <eero.leppalehto@gmail.com>
2023-05-16 14:49:39 +0300

    Add method for updating a row with mult values

--- pgModule.py
+++ pgModule.py
@@ -216,6 +216,51 @@ class DatabaseOperation():
             if self.errorCode == 0:
                 dbconnection.close()
 
+    # Method to update multiple values in a row
+    def updateManyValuesInRow(self, connectionArgs, table, columnValuePairs, limit):
+        """Updates a table
+        Args:
+            connectionArgs (dict): Connection arguments in key-value-pairs
+            table (str): Table name
+            column (str): Column to be updated
+            limit (str): WHERE SQL statement
+        """
+        server = connectionArgs['server']
+        port = connectionArgs['port']
+        database = connectionArgs['database']
+        user = connectionArgs['user']
+        password = connectionArgs['password']
+
+        try:
+            # Connect to the database and set error parameters
+            dbconnection = psycopg2.connect(
+                database=database, user=user, password=password, host=server, port=port)
+            self.errorCode = 0
+            self.errorMessage = 'Yhdistettiin tietokantaan'
+            self.detailedMessage = 'Connected to database successfully'
+
+            # Create a cursor to retrieve data from the table
+            with dbconnection.cursor() as cursor:
+                sqlClause = f'UPDATE {table} SET {columnValuePairs} WHERE {limit}'
+                cursor.execute(sqlClause)
+
+                # Set error values
+                self.errorCode = 0
+                self.errorMessage = 'Päivitettiin tietue onnistuneesti'
+                self.detailedMessage = 'Update was successful'
+                dbconnection.commit()
+                
+        except (Exception, psycopg2.Error )as error:
+
+            # Set error values 
+            self.errorCode = 1 # TODO: Design a set of error codes to use with this module
+            self.errorMessage = 'Tietokannan käsittely ei onnistunut'
+            self.detailedMessage = str(error)
+
+        finally:
+            if self.errorCode == 0:
+                dbconnection.close()
+
     # Method to delete a row from table
     def deleteFromTable(self, connectionArgs, table, limit):
         """Delete rows from a table using limiting SQL statement
