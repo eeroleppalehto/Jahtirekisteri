95cc86b839e8f926ff7ac73cb36a938b6d0fd457
eeroleppalehto <eero.leppalehto@gmail.com>
2023-05-16 14:51:07 +0300

    SQL for jasen_tila and ryhmat_jasenilla views

--- docs/SQL/jasen_tila.sql
+++ docs/SQL/jasen_tila.sql
@@ -14,3 +14,28 @@ ALTER TABLE public.jasen_tila
 
 GRANT ALL ON TABLE public.jasen_tila TO application;
 GRANT ALL ON TABLE public.jasen_tila TO postgres;
+
+-- View: public.ryhmat_jasenilla
+
+-- DROP VIEW public.ryhmat_jasenilla;
+
+CREATE OR REPLACE VIEW public.ryhmat_jasenilla
+ AS
+ SELECT jakoryhma.ryhman_nimi,
+    (jasen.sukunimi::text || ' '::text) || jasen.etunimi::text AS nimi,
+    jasenyys.liittyi,
+    jasenyys.poistui,
+    jasenyys.osuus
+   FROM jasenyys
+     JOIN jasen ON jasenyys.jasen_id = jasen.jasen_id
+     JOIN jakoryhma ON jakoryhma.ryhma_id = jasenyys.ryhma_id
+  ORDER BY jakoryhma.ryhman_nimi;
+
+ALTER TABLE public.ryhmat_jasenilla
+    OWNER TO postgres;
+COMMENT ON VIEW public.ryhmat_jasenilla
+    IS 'Näkymä joka näyttää ryhmä, jäsen, liittymispvm(,poistumispvm), osuus';
+
+GRANT ALL ON TABLE public.ryhmat_jasenilla TO application;
+GRANT ALL ON TABLE public.ryhmat_jasenilla TO postgres;
+
