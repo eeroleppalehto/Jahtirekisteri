701945484626ea434c6f132b4ae7054ecf9608a6
eeroleppalehto <eero.leppalehto@gmail.com>
2023-05-18 21:08:20 +0300

    Add method to get values with limit

--- pgModule.py
+++ pgModule.py
@@ -126,6 +126,57 @@ class DatabaseOperation():
             if self.errorCode == 0:
                 dbconnection.close()
 
+    # Method to get all rows from a given table with given limit
+
+    def getAllRowsFromTableWithLimit(self, connectionArgs, table, limit):
+        """Selects all rows from the table, view or SQL function's result set
+
+        Args:
+            connectionArgs (dict): Connection arguments in key-value pairs
+            table (str): Name of the table to read from
+            limit (str): Limit for the result set
+        """
+        server = connectionArgs['server']
+        port = connectionArgs['port']
+        database = connectionArgs['database']
+        user = connectionArgs['user']
+        password = connectionArgs['password']
+
+        try:
+            # Connect to the database and set error parameters
+            dbconnection = psycopg2.connect(
+                database=database, user=user, password=password, host=server, port=port)
+            self.errorCode = 0
+            self.errorMessage = 'Yhdistettiin tietokantaan'
+            self.detailedMessage = 'Connected to database successfully'
+
+            # Create a cursor to retrieve data from the table
+            with dbconnection.cursor() as cursor:
+                sqlClause = f"SELECT * FROM {table} WHERE {limit};"
+                cursor.execute(sqlClause)
+
+                # Set object properties
+                self.rows = cursor.rowcount
+                self.resultSet = cursor.fetchall()
+                self.columnHeaders = [cname[0] for cname in cursor.description]
+                self.columns = len(self.columnHeaders)
+
+                # Set error values
+                self.errorCode = 0
+                self.errorMessage = 'Luettiin taulu onnistuneesti'
+                self.detailedMessage = 'Read all data from the table'
+
+        except (Exception, psycopg2.Error )as error:
+
+            # Set error values
+            self.errorCode = 1
+            self.errorMessage = 'Tietokannan käsittely ei onnistunut'
+            self.detailedMessage = str(error)
+
+        finally:
+            if self.errorCode == 0:
+                dbconnection.close()
+
     # Method to insert a row to a given table
 
     def insertRowToTable(self, connectionArgs, sqlClause, returnId=False):
