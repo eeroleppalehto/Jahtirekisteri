d065df0ce2a36163386b5aa842126af40e25f6b2
eeroleppalehto <eero.leppalehto@gmail.com>
2023-08-22 14:07:16 +0300

    Types for RootStack

--- mobileApp/src/components/RootStack.tsx
+++ mobileApp/src/components/RootStack.tsx
@@ -4,26 +4,23 @@ import { getHeaderTitle } from "@react-navigation/elements";
 import BottomNav from "./BottomNav";
 import DetailsScreen from "./DetailsScreen";
 import { CustomNavigationBar } from "./CustomNavigationBar";
-    
+
 import { RootStackParamList } from "../NavigationTypes";
 
 const Stack = createNativeStackNavigator<RootStackParamList>();
 
 function RootStack() {
     return (
-        <Stack.Navigator 
+        <Stack.Navigator
             initialRouteName="BottomNavigation"
             screenOptions={{
                 header: (props) => <CustomNavigationBar {...props} />,
             }}
-            >
-            <Stack.Screen 
-                name="BottomNavigation" 
-                component={BottomNav}
-            />
+        >
+            <Stack.Screen name="BottomNavigation" component={BottomNav} />
             <Stack.Screen name="Details" component={DetailsScreen} />
         </Stack.Navigator>
-    )
+    );
 }
 
 export default RootStack;
