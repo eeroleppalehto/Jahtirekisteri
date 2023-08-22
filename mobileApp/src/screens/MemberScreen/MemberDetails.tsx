a96a945c97206a4c3d6fa9437e5166825e9e9771
eeroleppalehto <eero.leppalehto@gmail.com>
2023-08-22 14:07:36 +0300

    Types for MemberDetails

--- mobileApp/src/screens/MemberScreen/MemberDetails.tsx
+++ mobileApp/src/screens/MemberScreen/MemberDetails.tsx
@@ -1,34 +1,79 @@
 import { View } from "react-native";
+import { List, Divider, Text, Button, TextInput } from "react-native-paper";
+import { useState } from "react";
+
 import { Jasen } from "../../types";
-import { List, Divider, Text, Button } from "react-native-paper";
+import { MaintenanceTabScreenProps } from "../../NavigationTypes";
 
+type Props = MaintenanceTabScreenProps<"Jäsenet">;
 
-interface Props {
-    route: any;
-}
+function MemberDetails({ route, navigation }: Props) {
+    try {
+    } catch (error) {
+        return <Text>Virhe</Text>;
+    }
 
-function MemberDetails({ route }: Props) {
-    const jasen: Jasen = route.params.jasen;
+    if (!route.params) return <Text>Virhe</Text>;
+
+    const { data } = route.params;
+    const {
+        etunimi,
+        sukunimi,
+        jasen_id,
+        postitoimipaikka,
+        postinumero,
+        jakeluosoite,
+        tila,
+    } = data;
+    //const data: Jasen = route.params.data;
+
+    const [firstName, setfirstName] = useState<string>(etunimi);
 
     return (
         <View>
-            <Text variant="headlineLarge" style={{ margin: 15 }}>{jasen.etunimi} {jasen.sukunimi}</Text>
+            <Text variant="headlineLarge" style={{ margin: 15 }}>
+                {etunimi} {sukunimi}
+            </Text>
             <Divider bold={true} />
-            <List.Item title="Osoite" description={jasen.jakeluosoite} />
+            <List.Item title="Osoite" description={jakeluosoite} />
             <Divider />
-            <List.Item title="Postinumero" description={jasen.postinumero} />
+            <List.Item title="Postinumero" description={postinumero} />
             <Divider />
-            <List.Item title="Postitoimipaikka" description={jasen.postitoimipaikka} />
+            <List.Item
+                title="Postitoimipaikka"
+                description={postitoimipaikka}
+            />
             <Divider />
-            <List.Item title="Tila" description={jasen.tila} />
+            <List.Item title="Tila" description={tila} />
             <Divider />
-            <Button 
+            <TextInput
+                mode="outlined"
+                label="Etunimi"
+                value={firstName}
+                onChangeText={(text) => setfirstName(text)}
+                style={{ margin: 15 }}
+            />
+            <Button
                 mode="contained"
                 onPress={() => console.log("Pressed")}
                 disabled={true}
-                style={{ margin: 15, marginTop: 30 }}>
-                    MUOKKAA
+                style={{ margin: 15, marginTop: 30 }}
+            >
+                MUOKKAA
             </Button>
+            <View
+                style={{
+                    height: 40,
+                    borderRadius: 2,
+                    borderColor: "black",
+                    borderWidth: 1,
+                    margin: 15,
+                    padding: 5,
+                    backgroundColor: "white",
+                }}
+            >
+                <Text>Daa</Text>
+            </View>
         </View>
     );
 }
