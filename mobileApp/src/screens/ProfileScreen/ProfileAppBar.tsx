c5245bc0261093f3eef98c54d47a176b69dc54c5
eeroleppalehto <eero.leppalehto@gmail.com>
2023-08-22 14:08:16 +0300

    Types for ProfileAppBar

--- mobileApp/src/screens/ProfileScreen/ProfileAppBar.tsx
+++ mobileApp/src/screens/ProfileScreen/ProfileAppBar.tsx
@@ -1,11 +1,16 @@
-import { Appbar } from 'react-native-paper';
-import { DrawerNavigationProp } from '@react-navigation/drawer';
+import { Appbar } from "react-native-paper";
+import { DrawerNavigationProp } from "@react-navigation/drawer";
+import { DrawerNavParamList } from "../../components/DrawerNav";
+import { DrawerHeaderProps } from "@react-navigation/drawer";
 
-interface Props {
-    navigation: DrawerNavigationProp<any, any>;
-}
+type Props = DrawerHeaderProps;
 
-export default function ProfileAppBar({ navigation }: Props) {
+export default function ProfileAppBar({
+    navigation,
+    route,
+    layout,
+    options,
+}: Props) {
     return (
         <Appbar.Header>
             <Appbar.BackAction onPress={() => navigation.goBack()} />
