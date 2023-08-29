d311680f76a9db4b9ef0b6a80c1cfd4a7e12eb66
AleksanteriLohja <97512082+AleksanteriLohja@users.noreply.github.com>
2023-08-29 16:06:04 +0300

    Add jakoryhmaRouter, jakoryhmaService, and related utilities; update package.json and package-lock.json

--- server/index.ts
+++ server/index.ts
@@ -1,32 +1,29 @@
-// THIS IS THE ENTRY POINT FOR THE SERVER
-// =======================================
-
-// IMPORTS
-// =======================================
+// Import the required modules
 import express from 'express';
 import cors from 'cors';
 import jasenRouter from './routers/jasenRouter';
+import jakoryhmaRouter from './routers/jakoryhmaRouter';
 
-// Initialize express app
+// Initialize the Express application
 const app = express();
 
-// Middleware
+// Middleware configurations
+// Enable CORS (Cross-Origin Resource Sharing) to allow frontend to connect
 app.use(cors());
+// Enable the parsing of incoming JSON payloads in request bodies
 app.use(express.json());
 
-// Listen to port 3000
+// Set the port to listen on
 const PORT = 3000;
 
-// Routes
-app.get('/ping', (_req, res) => {
-    console.log('someone pinged here');
-    res.send('pong');
-});
-
-// Mount the jasenRouter to the /api/jasenet route
+// Routing
+// Mount the jasenRouter for handling member-related routes under /api/members
 app.use('/api/members', jasenRouter);
+// Mount the jakoryhmaRouter for handling group-related routes under /api/jakoryhma
+app.use('/api/jakoryhma', jakoryhmaRouter);
 
-// Start the server
+// Start the Express server and listen for incoming requests
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+  // Log to console when the server is successfully running
+  console.log(`Server running on port ${PORT}`);
 });
