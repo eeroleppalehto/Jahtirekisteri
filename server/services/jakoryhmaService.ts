08021cc8acb3d77d7b5c48543a0b58baf6a3d81b
eeroleppalehto <eero.leppalehto@gmail.com>
2023-09-01 11:46:43 +0300

    Import client from client.ts instead of generating a new one

--- server/services/jakoryhmaService.ts
+++ server/services/jakoryhmaService.ts
@@ -1,46 +1,47 @@
+import prisma from "../client";
 import { JakoryhmaSchema } from "../zodSchemas/jakoryhmaZod";
 
 export const createJakoryhma = async (data: unknown) => {
     const parsedData = JakoryhmaSchema.parse(data);
 
-  return await prisma.jakoryhma.create({
-    data: {
+    return await prisma.jakoryhma.create({
+        data: {
             seurue_id: parsedData.seurue_id,
             ryhman_nimi: parsedData.ryhman_nimi,
-    },
-  });
+        },
+    });
 };
 
 export const getAllJakoryhma = async () => {
-  return await prisma.jakoryhma.findMany();
+    return await prisma.jakoryhma.findMany();
 };
 
 export const readJakoryhma = async (id: number) => {
-  return await prisma.jakoryhma.findUnique({
-    where: {
-      ryhma_id: id,
-    },
-  });
+    return await prisma.jakoryhma.findUnique({
+        where: {
+            ryhma_id: id,
+        },
+    });
 };
 
 export const updateJakoryhma = async (id: number, data: unknown) => {
     const parsedData = JakoryhmaSchema.parse(data);
 
-  return await prisma.jakoryhma.update({
-    where: {
-      ryhma_id: id,
-    },
-    data: {
+    return await prisma.jakoryhma.update({
+        where: {
+            ryhma_id: id,
+        },
+        data: {
             seurue_id: parsedData.seurue_id,
             ryhman_nimi: parsedData.ryhman_nimi,
-    },
-  });
+        },
+    });
 };
 
 export const deleteJakoryhma = async (id: number) => {
-  return await prisma.jakoryhma.delete({
-    where: {
-      ryhma_id: id,
-    },
-  });
+    return await prisma.jakoryhma.delete({
+        where: {
+            ryhma_id: id,
+        },
+    });
 };
