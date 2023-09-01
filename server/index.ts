29262111bf55fb1d1b516abcf95c6900ed0e3ccb
eeroleppalehto <eero.leppalehto@gmail.com>
2023-09-01 11:43:47 +0300

    Fixes in errorHandler

--- server/index.ts
+++ server/index.ts
@@ -1,10 +1,10 @@
-import express, { Request, Response } from 'express';
-import cors from 'cors';
-import 'express-async-errors';
-import jasenRouter from './routers/jasenRouter';
-import kaatoRouter from './routers/kaatoRouter';
-import jakoryhmaRouter from './routers/jakoryhmaRouter';
-import { ZodError } from 'zod';
+import express, { NextFunction, Request, Response } from "express";
+import cors from "cors";
+import "express-async-errors";
+import jasenRouter from "./routers/jasenRouter";
+import kaatoRouter from "./routers/kaatoRouter";
+import jakoryhmaRouter from "./routers/jakoryhmaRouter";
+import { ZodError } from "zod";
 
 // Initialize the Express application
 const app = express();
@@ -16,30 +16,34 @@ app.use(cors());
 app.use(express.json());
 
 // Routes
-app.get('/ping', (_req, res) => {
-    console.log('someone pinged here');
-    res.send('pong');
+app.get("/ping", (_req, res) => {
+    console.log("someone pinged here");
+    res.send("pong");
 });
 
 // Mount the jasenRouter, kaatoRouter to the /api/ path
-app.use('/api/members', jasenRouter);
-app.use('/api/jakoryhma', jakoryhmaRouter);
-app.use('/api/shots', kaatoRouter);
+app.use("/api/members", jasenRouter);
+app.use("/api/jakoryhma", jakoryhmaRouter);
+app.use("/api/shots", kaatoRouter);
 
 // Custom error handler
-const errorHandler = (error: any, _req: Request, res: Response) => {
-  let errorMessage = "Error occurred: ";
+const errorHandler = (
+    error: any,
+    _req: Request,
+    res: Response,
+    next: NextFunction
+) => {
+    let errorMessage = "Error occurred: ";
 
-  if (error instanceof Error) {
-    errorMessage += error.message;
-    return res.status(400).send({ error: errorMessage });
-  } else if (error instanceof ZodError) {
-    errorMessage += error.flatten();
-    return res.status(400).json({ error: errorMessage });
-  } else {
-    errorMessage += "An unknown error occurred.";
-    return res.status(500).send({ error: errorMessage });
-  }
+    if (error instanceof Error) {
+        errorMessage += error.message;
+        return res.status(400).send({ error: errorMessage });
+    } else if (error instanceof ZodError) {
+        errorMessage += error.flatten();
+        return res.status(400).json({ error: errorMessage });
+    }
+
+    next(error);
 };
 
 // Centralized error handling
@@ -50,5 +54,5 @@ const PORT = 3000;
 
 // Start the Express server and listen for incoming requests
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+    console.log(`Server running on port ${PORT}`);
 });
