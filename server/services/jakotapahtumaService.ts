aaefa7065b1421ebb9adf9281a37540c15ad5518
AleksanteriLohja <97512082+AleksanteriLohja@users.noreply.github.com>
2023-09-21 15:08:23 +0300

    Enhance with Zod and naming

--- server/services/jakotapahtumaService.ts
+++ server/services/jakotapahtumaService.ts
@@ -1,48 +1,25 @@
-// Import PrismaClient for database operations and Zod for schema validation
-import { PrismaClient } from '@prisma/client';
 import jakotapahtumaZod from '../zodSchemas/jakotapahtumaZod';
 import { z } from 'zod';
+import prisma from '../client';
 
-// Initialize a new PrismaClient instance
-const prisma = new PrismaClient();
-
-// Define the TypeScript type for Jakotapahtuma based on the Zod schema
 type jakotapahtumaType = z.infer<typeof jakotapahtumaZod>;
 
-// Create a flexible type that can handle either string or Date types for the 'paiva' field
-type jakotapahtumaTypeWithFlexibleDate = Omit<jakotapahtumaType, 'paiva'> & { paiva: string | Date };
-
-// Function to create a new Jakotapahtuma entry
-export const create = async (data: jakotapahtumaType) => {
-    // Prepare new data while handling string or Date types for 'paiva'
-    const newData: jakotapahtumaTypeWithFlexibleDate = { ...data };
-    if (typeof newData.paiva === 'string') {
-        newData.paiva = new Date(newData.paiva);
-    }
-    return await prisma.jakotapahtuma.create({ data: newData as jakotapahtumaType });
+export const createJakotapahtuma = async (data: jakotapahtumaType) => {
+    return await prisma.jakotapahtuma.create({ data });
 };
 
-// Function to read a Jakotapahtuma entry by its ID
-export const read = async (id: number) => {
+export const readJakotapahtumaById = async (id: number) => {
     return await prisma.jakotapahtuma.findUnique({ where: { tapahtuma_id: id } });
 };
 
-// Function to update an existing Jakotapahtuma entry
-export const update = async (id: number, data: Partial<jakotapahtumaType>) => {
-    // Prepare new data while handling string or Date types for 'paiva'
-    const newData: Partial<jakotapahtumaTypeWithFlexibleDate> = { ...data };
-    if (newData.paiva && typeof newData.paiva === 'string') {
-        newData.paiva = new Date(newData.paiva);
-    }
-    return await prisma.jakotapahtuma.update({ where: { tapahtuma_id: id }, data: newData as Partial<jakotapahtumaType> });
+export const updateJakotapahtumaById = async (id: number, data: Partial<jakotapahtumaType>) => {
+    return await prisma.jakotapahtuma.update({ where: { tapahtuma_id: id }, data });
 };
 
-// Function to delete a Jakotapahtuma entry
-export const del = async (id: number) => {
+export const deleteJakotapahtumaById = async (id: number) => {
     return await prisma.jakotapahtuma.delete({ where: { tapahtuma_id: id } });
 };
 
-// Newly added function to retrieve all Jakotapahtuma entries
-export const getAll = async () => {
+export const getAllJakotapahtumat = async () => {
     return await prisma.jakotapahtuma.findMany();
 };
