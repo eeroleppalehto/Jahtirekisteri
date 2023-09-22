10d5584f4bb79190ed32b81c90da860b455539aa
AleksanteriLohja <97512082+AleksanteriLohja@users.noreply.github.com>
2023-09-22 12:09:08 +0300

    Update kaadonkasittelyRouter.ts
    Refactor kaadonkasittelyRouter for ID validation and centralized error handling

--- server/routers/kaadonkasittelyRouter.ts
+++ server/routers/kaadonkasittelyRouter.ts
@@ -1,54 +1,40 @@
-/* eslint-disable @typescript-eslint/no-misused-promises */
-import express from 'express';
+import express from "express";
 import {
     createKaadonkasittely,
     readKaadonkasittelyById,
     updateKaadonkasittelyById,
     deleteKaadonkasittelyById
-} from '../services/kaadonkasittelyService';
+} from "../services/kaadonkasittelyService";
 
 const router = express.Router();
 
-router.post('/', async (req, res) => {
-    try {
-        const newKaadonkasittely = await createKaadonkasittely(req.body);
-        res.json(newKaadonkasittely);
-    } catch (error) {
-        res.status(400).send('Invalid data');
-    }
+router.post("/", async (req, res) => {
+    const newKaadonkasittely = await createKaadonkasittely(req.body);
+    res.json(newKaadonkasittely);
 });
 
-router.get('/:id', async (req, res) => {
-    const id = parseInt(req.params.id, 10);
-    if (Number.isNaN(id)) throw new Error('Invalid Id');
-    try {
-        const kaadonkasittely = await readKaadonkasittelyById(id);
-        res.json(kaadonkasittely);
-    } catch (error) {
-        res.status(400).send('Invalid ID');
+router.get("/:id", async (req, res) => {
+    if (Number.isNaN(Number(req.params.id))) {
+        return res.status(400).send("Invalid ID");
     }
+    const kaadonkasittely = await readKaadonkasittelyById(Number(req.params.id));
+    res.json(kaadonkasittely);
 });
 
-router.put('/:id', async (req, res) => {
-    const id = parseInt(req.params.id, 10);
-    if (Number.isNaN(id)) throw new Error('Invalid Id');
-    try {
-        const updatedKaadonkasittely = await updateKaadonkasittelyById(id, req.body);
-        res.json(updatedKaadonkasittely);
-    } catch (error) {
-        res.status(400).send('Invalid data or ID');
+router.put("/:id", async (req, res) => {
+    if (Number.isNaN(Number(req.params.id))) {
+        return res.status(400).send("Invalid ID");
     }
+    const updatedKaadonkasittely = await updateKaadonkasittelyById(Number(req.params.id), req.body);
+    res.json(updatedKaadonkasittely);
 });
 
-router.delete('/:id', async (req, res) => {
-    const id = parseInt(req.params.id, 10);
-    if (Number.isNaN(id)) throw new Error('Invalid Id');
-    try {
-        const deletedKaadonkasittely = await deleteKaadonkasittelyById(id);
-        res.json(deletedKaadonkasittely);
-    } catch (error) {
-        res.status(400).send('Invalid ID');
+router.delete("/:id", async (req, res) => {
+    if (Number.isNaN(Number(req.params.id))) {
+        return res.status(400).send("Invalid ID");
     }
+    const deletedKaadonkasittely = await deleteKaadonkasittelyById(Number(req.params.id));
+    res.json(deletedKaadonkasittely);
 });
 
 export default router;
