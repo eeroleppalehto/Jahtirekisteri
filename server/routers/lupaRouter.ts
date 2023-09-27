9d65b048227882cb57308b6a378e7fcfdb010311
AleksanteriLohja <97512082+AleksanteriLohja@users.noreply.github.com>
2023-09-27 12:54:09 +0300

    Update lupaRouter.ts
    Corrected ID validation and centralized Error Handling

--- server/routers/lupaRouter.ts
+++ server/routers/lupaRouter.ts
@@ -13,51 +13,37 @@ const router = express.Router();
 
 // POST endpoint to create a new 'lupa' record
 router.post("/", async (req, res) => {
-    // Use the createLupa function from lupaService to add a new 'lupa'
     const newLupa = await createLupa(req.body);
-    // Send the newly created 'lupa' as the response
     res.json(newLupa);
 });
 
 // GET endpoint to fetch a 'lupa' by its ID
 router.get("/:id", async (req, res) => {
-    // Convert the ID from string to number
     const id = Number(req.params.id);
-    // Validate the ID
     if (!Number.isInteger(id)) {
-        return res.status(400).send("Invalid ID");
+        throw new Error("Invalid ID");
     }
-    // Use readLupaById function to fetch the 'lupa' by its ID
     const lupa = await readLupaById(id);
-    // Send the fetched 'lupa' as the response
     res.json(lupa);
 });
 
 // PUT endpoint to update a 'lupa' by its ID
 router.put("/:id", async (req, res) => {
-    // Convert the ID from string to number
     const id = Number(req.params.id);
-    // Validate the ID
     if (!Number.isInteger(id)) {
-        return res.status(400).send("Invalid ID");
+        throw new Error("Invalid ID");
     }
-    // Use updateLupaById function to update the 'lupa' by its ID and data
     const updatedLupa = await updateLupaById(id, req.body);
-    // Send the updated 'lupa' as the response
     res.json(updatedLupa);
 });
 
 // DELETE endpoint to remove a 'lupa' by its ID
 router.delete("/:id", async (req, res) => {
-    // Convert the ID from string to number
     const id = Number(req.params.id);
-    // Validate the ID
     if (!Number.isInteger(id)) {
-        return res.status(400).send("Invalid ID");
+        throw new Error("Invalid ID");
     }
-    // Use deleteLupaById function to remove the 'lupa' by its ID
     const deletedLupa = await deleteLupaById(id);
-    // Send the deleted 'lupa' as the response
     res.json(deletedLupa);
 });
 
