94a44a89cb3dfc7dcaa89acf2a00caf6f2443aa4
eeroleppalehto <eero.leppalehto@gmail.com>
2023-10-04 16:14:18 +0300

    Update addMeberShareTab for updated schema

--- desktopApp/tabs/shareMemberTabWidget.py
+++ desktopApp/tabs/shareMemberTabWidget.py
@@ -16,7 +16,6 @@ class Ui_shareMemberTabWidget(QScrollArea, QWidget):
     def __init__(self):
         super(Ui_shareMemberTabWidget, self).__init__()
         loadUi('ui/shareMemberTab.ui', self)
-        #TODO: Do everything lol
     
         self.currentDate = date.today()
 
@@ -24,6 +23,9 @@ class Ui_shareMemberTabWidget(QScrollArea, QWidget):
         self.shareDE: QDateEdit = self.shareDateEdit
         self.sharePortionCB: QComboBox = self.portionComboBox
         self.shareMemberCB: QComboBox = self.memberComboBox
+        self.sharePartyCB: QComboBox = self.partyComboBox
+        self.sharePartyCB.currentIndexChanged.connect(self.handlePartyCBChange)
+        
         self.shareSavePushBtn: QPushButton = self.shareSavePushButton
         self.shareSavePushBtn.clicked.connect(self.saveShare) # Signal
         self.sharedPortionsTW: QTableWidget = self.shareSharedPortionsTableWidget
@@ -93,21 +95,6 @@ class Ui_shareMemberTabWidget(QScrollArea, QWidget):
             self.sharePortionText = prepareData.prepareComboBox(
                 databaseOperation2, self.sharePortionCB, 0, 0)
         
-        # Prepare portion combo box
-        databaseOperation3 = pgModule.DatabaseOperation()
-        databaseOperation3.getAllRowsFromTable(
-            self.connectionArguments, 'public.nimivalinta')
-        if databaseOperation3.errorCode != 0:
-            self.alert(
-                'Vakava virhe',
-                'Tietokantaoperaatio epäonnistui',
-                databaseOperation3.errorMessage,
-                databaseOperation3.detailedMessage
-                )
-        else:
-            self.shareMemberIdList = prepareData.prepareComboBox(
-                databaseOperation3, self.shareMemberCB, 1, 0)
-        
         # Prepare the shared portions table
         databaseOperation4 = pgModule.DatabaseOperation()
         databaseOperation4.getAllRowsFromTable(
@@ -121,7 +108,6 @@ class Ui_shareMemberTabWidget(QScrollArea, QWidget):
                 )
         else:
             try:
-                print("moi")
                 # parse the data from the view to readable format
                 tableData = prepareData.parseSharedPortionOfShot(databaseOperation4.resultSet)
                 
@@ -140,6 +126,38 @@ class Ui_shareMemberTabWidget(QScrollArea, QWidget):
                 'Oops'
             )
             
+        
+        databaseOperation5 = pgModule.DatabaseOperation()
+        databaseOperation5.getAllRowsFromTableWithLimit(
+            self.connectionArguments, 'public.seurue', 'seurue_tyyppi_id = 2')
+        if databaseOperation5.errorCode != 0:
+            self.alert(
+                'Vakava virhe',
+                'Tietokantaoperaatio epäonnistui',
+                databaseOperation5.errorMessage,
+                databaseOperation5.detailedMessage
+                )
+        else:
+            self.sharePartyText = prepareData.prepareComboBox(
+                databaseOperation5, self.sharePartyCB, 2, 0)
+            
+            for i in range(len(self.sharePartyText)):
+                self.sharePartyCB.setItemData(i, databaseOperation5.resultSet[i])
+                
+        databaseOperation6 = pgModule.DatabaseOperation()
+        databaseOperation6.getAllRowsFromTable(
+            self.connectionArguments, 'public.jasenyys_nimella')
+        if databaseOperation6.errorCode != 0:
+            self.alert(
+                'Vakava virhe',
+                'Tietokantaoperaatio epäonnistui',
+                databaseOperation6.errorMessage,
+                databaseOperation6.detailedMessage
+                )
+        else:
+            self.membershipData = databaseOperation6.resultSet
+            self.handlePartyCBChange()
+        
         
         # Set the chosen shot label to empty
         self.chosenItemLbl.setText("Ei valittua kaatoa")
@@ -176,12 +194,11 @@ class Ui_shareMemberTabWidget(QScrollArea, QWidget):
             shareDay = self.shareDE.date().toPyDate()
             portion = self.sharePortionCB.currentText()
             weight = portionDict[portion] * self.shotWeight
-            shareMemberChosenItemIx = self.shareMemberCB.currentIndex()
-            shareMemberId = self.shareMemberIdList[shareMemberChosenItemIx]
+            shareMembership = self.shareMemberCB.currentData()
             
             # Create SQL query
-            sqlClauseBeginning = f"INSERT INTO public.jakotapahtuma_jasen(paiva, kaadon_kasittely_id, osnimitys, jasen_id, maara) VALUES("
-            sqlClauseValues = f"'{shareDay}', {shotUsageId!r}, {portion!r}, {shareMemberId!r}, {weight!r}"
+            sqlClauseBeginning = f"INSERT INTO public.jakotapahtuma_jasen(paiva, kaadon_kasittely_id, osnimitys, jasenyys_id, maara) VALUES("
+            sqlClauseValues = f"'{shareDay}', {shotUsageId!r}, {portion!r}, {shareMembership!r}, {weight!r}"
             sqlClauseEnd = ");"
             
             # Assemble the query
@@ -210,4 +227,17 @@ class Ui_shareMemberTabWidget(QScrollArea, QWidget):
             self.populateSharePage()
             self.shareSavePushBtn.setEnabled(False)
    
-         
+    def handlePartyCBChange(self):
+        """Method for handling the change event on the party combo box
+        """
+        if self.sharePartyCB.currentData() == None:
+            return
+        
+        currentPartyId = self.sharePartyCB.currentData()[0]
+        
+        self.shareMemberCB.clear()
+        
+        for i, item in enumerate(self.membershipData):
+            if item[2] == currentPartyId:
+                # (member name, membership id)
+                self.shareMemberCB.addItem(item[1], item[0])
