8a3da980b81700d145fdac31969f5a8b0dc20839
eeroleppalehto <eero.leppalehto@gmail.com>
2023-10-24 10:34:59 +0300

    Updated router module

--- server/routers/apiViewRouter.ts
+++ server/routers/apiViewRouter.ts
@@ -1,25 +1,23 @@
 /* eslint-disable @typescript-eslint/no-unsafe-call */
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
 // Import necessary modules and services
-import express from 'express';
-import { getViewData } from '../services/apiViewService';
-import viewValidationZod from '../zodSchemas/viewValidationZod';
+import express from "express";
+import { getViewData } from "../services/apiViewService";
+// import viewValidationZod from '../zodSchemas/viewValidationZod';
 
 // Initialize an express router
 const router = express.Router();
 
 // Define GET route for fetching view data
-router.get('/', (async (req, res) => {
-    const viewName = req.query.viewName as string;
-    const validationResult = viewValidationZod.safeParse(viewName);
-    
-    if (!validationResult.success) {
-        res.status(400).json({ message: 'Invalid view name' });
-        return;
+router.get("/", (async (req, res) => {
+    const viewName = req.query.viewName;
+
+    if (typeof viewName !== "string") {
+        throw new Error("Invalid view name");
     }
 
     const data = await getViewData(viewName);
-    res.status(200).json(data);  // 200 OK
+    res.status(200).json(data); // 200 OK
 }) as express.RequestHandler);
 
 // Export the router for use in other modules
