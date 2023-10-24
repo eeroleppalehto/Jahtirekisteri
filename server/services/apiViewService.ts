f1da8298d173181483e14eaba30873d0ac80400a
eeroleppalehto <eero.leppalehto@gmail.com>
2023-10-24 10:38:12 +0300

    Fixed bug relating to parsing bigint data type

--- server/services/apiViewService.ts
+++ server/services/apiViewService.ts
@@ -13,11 +13,24 @@ import viewValidationZod from "../zodSchemas/viewValidationZod";
 export const getViewData = async (viewName: string): Promise<object[] | []> => {
     // Validate the view name
     const parsedViewName = viewValidationZod.parse(viewName);
-    
+
     // Construct the query
     const query = `SELECT * FROM ${parsedViewName}`;
 
     // Execute the query
     const data = await prisma.$queryRawUnsafe<object[]>(query);
 
+    // There is a bug that causes bigint values to be returned as BigInt objects instead of strings.
+    // The following code parses the data to convert bigint values to strings
+    const parsedData: object[] = data.map((row) => {
+        const jsonString = JSON.stringify(row, (_key, value) => {
+            return typeof value === "bigint" ? value.toString() : value;
+        });
+
+        const obj: object = JSON.parse(jsonString);
+
+        return obj;
+    });
+
+    return parsedData;
 };
