db01e9049e9f0b313552191096df60c472eb6517
eeroleppalehto <eero.leppalehto@gmail.com>
2023-10-30 14:03:13 +0200

    Added icons to BottoNav and added Jako tab

--- mobileApp/src/components/BottomNav.tsx
+++ mobileApp/src/components/BottomNav.tsx
@@ -1,10 +1,12 @@
 import { createMaterialBottomTabNavigator } from "@react-navigation/material-bottom-tabs";
 import { Text } from "react-native-paper";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 
 import MaintenanceNav from "./MaintenanceNav";
 import { BottomNavParamList } from "../NavigationTypes";
 
 import ChartVictoryXL from "../screens/GraphScreen/ChartVictoryXL";
+import ShotScreen from "../screens/ShotScreen";
 
 const Tab = createMaterialBottomTabNavigator<BottomNavParamList>();
 
@@ -15,9 +17,58 @@ const Placeholder = () => {
 function BottomNav() {
     return (
         <Tab.Navigator>
-            <Tab.Screen name="Grafiikka" component={ChartVictoryXL} />
-            <Tab.Screen name="Kaadot" component={Placeholder} />
-            <Tab.Screen name="Ylläpito" component={MaintenanceNav} />
+            <Tab.Screen
+                name="Grafiikka"
+                component={ChartVictoryXL}
+                options={{
+                    tabBarIcon: ({ color }) => (
+                        <MaterialCommunityIcons
+                            name="chart-box-outline"
+                            size={24}
+                            color={color}
+                        />
+                    ),
+                }}
+            />
+            <Tab.Screen
+                name="Kaadot"
+                component={ShotScreen}
+                options={{
+                    tabBarIcon: ({ color }) => (
+                        <MaterialCommunityIcons
+                            name="crosshairs"
+                            size={24}
+                            color={color}
+                        />
+                    ),
+                }}
+            />
+            <Tab.Screen
+                name="Jako"
+                component={Placeholder}
+                options={{
+                    tabBarIcon: ({ color }) => (
+                        <MaterialCommunityIcons
+                            name="share"
+                            size={24}
+                            color={color}
+                        />
+                    ),
+                }}
+            />
+            <Tab.Screen
+                name="Ylläpito"
+                component={MaintenanceNav}
+                options={{
+                    tabBarIcon: ({ color }) => (
+                        <MaterialCommunityIcons
+                            name="account-group"
+                            size={24}
+                            color={color}
+                        />
+                    ),
+                }}
+            />
         </Tab.Navigator>
     );
 }
