680e31b3cc5ddccf57fa845fae7bc31e9fb737cb
AleksanteriLohja <97512082+AleksanteriLohja@users.noreply.github.com>
2023-10-30 11:46:11 +0200

    Moved errorHandler to separate utils module

--- server/index.ts
+++ server/index.ts
@@ -1,5 +1,5 @@
 // Import required libraries and modules
-import express, { NextFunction, Request, Response } from "express";
+import express from "express";
 import cors from "cors";
 import "express-async-errors";
 import jasenRouter from "./routers/jasenRouter";
@@ -12,8 +12,7 @@ import seurueRouter from "./routers/seurueRouter";
 import jasenyysRouter from "./routers/jasenyysRouter";
 import apiViewRouter from "./routers/apiViewRouter";
 import optionTablesRouter from "./routers/optionTablesRouter";
-import { ZodError } from "zod";
-import { PrismaClientKnownRequestError } from "@prisma/client/runtime/library";
+import { errorHandler } from './utils/middleware';  // Import errorHandler
 
 // Initialize the Express application
 const app = express();
@@ -34,61 +33,12 @@ app.use("/api/members", jasenRouter);
 app.use("/api/jakoryhma", jakoryhmaRouter);
 app.use("/api/shots", kaatoRouter);
 app.use("/api/jakotapahtuma", jakotapahtumaRouter);
-app.use("/api/kaadonkasittely", kaadonkasittelyRouter); // New router added
+app.use("/api/kaadonkasittely", kaadonkasittelyRouter);
 app.use("/api/lupa", lupaRouter);
 app.use("/api/seurue", seurueRouter);
 app.use("/api/jasenyys", jasenyysRouter);
 app.use("/api/view", apiViewRouter);
 app.use("/api/option-tables", optionTablesRouter);
-
-type ErrorType = {
-    success: boolean;
-    errorType: string;
-    errorMessage: string;
-    errorDetails: string[] | unknown[];
-};
-
-// Custom error handler
-const errorHandler = (
-    error: unknown,
-    _req: Request,
-    res: Response,
-    next: NextFunction
-) => {
-    // Conditional error handling
-    if (error instanceof ZodError) {
-        const returnObject: ErrorType = {
-            success: false,
-            errorType: "ZodError",
-            errorMessage: "Invalid input",
-            errorDetails: [],
-        };
-
-        error.issues.forEach((issue) => {
-            returnObject.errorDetails.push(`${issue.path}: ${issue.message}`);
-        });
-
-        return res.status(400).json(returnObject);
-    } else if (error instanceof PrismaClientKnownRequestError) {
-        const returnObject: ErrorType = {
-            success: false,
-            errorType: "PrismaClientKnownRequestError",
-            errorMessage: "Error in database query",
-            errorDetails: [error.code, error.meta],
-        };
-        return res.status(400).send(returnObject);
-    } else if (error instanceof Error) {
-        const returnObject: ErrorType = {
-            success: false,
-            errorType: "Error",
-            errorMessage: error.message,
-            errorDetails: [],
-        };
-        return res.status(400).send(returnObject);
-    }
-
-    next(error);
-};
 
 // Centralized error handling
 app.use(errorHandler);
