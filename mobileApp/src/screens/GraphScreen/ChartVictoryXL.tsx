3729c33cc4480f3c3b5fa0113845f7af974a5e25
eeroleppalehto <eero.leppalehto@gmail.com>
2023-11-01 15:11:52 +0200

    Added fonts to the graph

--- mobileApp/src/screens/GraphScreen/ChartVictoryXL.tsx
+++ mobileApp/src/screens/GraphScreen/ChartVictoryXL.tsx
@@ -4,44 +4,63 @@ import { LinearGradient, useFont, vec } from "@shopify/react-native-skia";
 // import inter from "../fonts/inter-medium.ttf"
 
 const ChartVictoryXL = () => {
-    //const font = useFont(inter, 12)
+    const font = useFont(
+        require("../../fonts/Roboto-Regular.ttf"),
+        12,
+        (err) => {
+            console.log(err.message);
+        }
+    );
+
     const data = Array.from({ length: 6 }, (_, index) => ({
         month: index + 1,
         listenCount: Math.floor(Math.random() * (100 - 50 + 1)) + 50,
     }));
 
     return (
-        <CartesianChart
-            data={data}
-            xKey="month"
-            yKeys={["listenCount"]}
-            domainPadding={{ left: 50, right: 50, top: 30 }}
-            axisOptions={{
-                formatXLabel(value) {
-                    const date = new Date(2023, value - 1);
-                    return date.toLocaleString("default", {
-                        month: "short",
-                    });
-                },
+        <View
+            style={{
+                flex: 1,
+                justifyContent: "space-between",
+                //alignItems: "center",
+                width: "100%",
+                height: "90%",
+                padding: "2%",
             }}
         >
-            {({ points, chartBounds }) => (
-                <Bar
-                    chartBounds={chartBounds}
-                    points={points.listenCount}
-                    roundedCorners={{
-                        topLeft: 5,
-                        topRight: 5,
-                    }}
-                >
-                    <LinearGradient
-                        start={vec(0, 0)}
-                        end={vec(0, 400)}
-                        colors={["#a78bfa", "#a78bfa50"]}
-                    />
-                </Bar>
-            )}
-        </CartesianChart>
+            <CartesianChart
+                data={data}
+                xKey="month"
+                yKeys={["listenCount"]}
+                domainPadding={{ left: 50, right: 50, top: 30 }}
+                axisOptions={{
+                    font,
+                    formatXLabel(value) {
+                        const date = new Date(2023, value - 1);
+                        return date.toLocaleString("default", {
+                            month: "short",
+                        });
+                    },
+                }}
+            >
+                {({ points, chartBounds }) => (
+                    <Bar
+                        chartBounds={chartBounds}
+                        points={points.listenCount}
+                        roundedCorners={{
+                            topLeft: 5,
+                            topRight: 5,
+                        }}
+                    >
+                        <LinearGradient
+                            start={vec(0, 0)}
+                            end={vec(0, 400)}
+                            colors={["#a78bfa", "#a78bfa50"]}
+                        />
+                    </Bar>
+                )}
+            </CartesianChart>
+        </View>
     );
 };
 
