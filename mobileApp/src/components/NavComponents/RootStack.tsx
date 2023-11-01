f2f7c2f3ab0259a6c73944c53e00dbac7abd9014
eeroleppalehto <eero.leppalehto@gmail.com>
2023-11-01 15:10:50 +0200

    Updated imports

--- mobileApp/src/components/NavComponents/RootStack.tsx
+++ mobileApp/src/components/NavComponents/RootStack.tsx
@@ -2,9 +2,9 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { getHeaderTitle } from "@react-navigation/elements";
 
 import BottomNav from "./BottomNav";
-import DetailsScreen from "../DetailsScreen";
+import DetailsScreen from "../../screens/Details/DetailsScreen";
 import { CustomAppBar } from "../AppBars/CustomAppBar";
-import FormScreen from "../FormScreen";
+import FormScreen from "../../screens/Forms/FormScreen";
 import FormAppBar from "../AppBars/FormAppBar";
 
 import { NativeStackHeaderProps } from "@react-navigation/native-stack";
