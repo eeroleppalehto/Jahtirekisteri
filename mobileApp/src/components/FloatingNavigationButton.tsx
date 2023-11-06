34047350a509623d3b1a5fb7458035d5bbd16b55
eeroleppalehto <eero.leppalehto@gmail.com>
2023-11-06 13:21:41 +0200

    Floating button to open forms

---
+++ mobileApp/src/components/FloatingNavigationButton.tsx
@@ -0,0 +1,52 @@
+import {
+    StyleProp,
+    ViewStyle,
+    Animated,
+    StyleSheet,
+    Platform,
+    I18nManager,
+} from "react-native";
+import { AnimatedFAB } from "react-native-paper";
+import { useNavigation } from "@react-navigation/native";
+
+interface Props {
+    scrollValue: number;
+    type: string;
+    label: string;
+}
+
+const FloatingNavigationButton = ({ scrollValue, type, label }: Props) => {
+    const extended = scrollValue <= 10;
+    const animateFrom = "right";
+    const fabStyle = { [animateFrom]: 16 };
+
+    const isIOS = Platform.OS === "ios";
+
+    const navigation = useNavigation();
+
+    return (
+        <AnimatedFAB
+            icon={"plus"}
+            label={label}
+            extended={extended}
+            onPress={() => navigation.navigate("Forms", { type })}
+            visible={true}
+            animateFrom={"right"}
+            iconMode={"static"}
+            style={[styles.fabStyle, {}, { right: 16 }]}
+        />
+    );
+};
+
+export default FloatingNavigationButton;
+
+const styles = StyleSheet.create({
+    container: {
+        flexGrow: 1,
+    },
+    fabStyle: {
+        bottom: 16,
+        right: 16,
+        position: "absolute",
+    },
+});
