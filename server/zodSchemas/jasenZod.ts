184ff1225907434c6425470d4c304a46dee2272c
eeroleppalehto <eero.leppalehto@gmail.com>
2023-11-14 10:31:27 +0200

    Updated the jasen validation

--- server/zodSchemas/jasenZod.ts
+++ server/zodSchemas/jasenZod.ts
@@ -12,9 +12,9 @@ const validateTila = (tila: string): boolean => {
 export const jasenInput = z.object({
     etunimi: z.string().max(50),
     sukunimi: z.string().max(50),
-    jakeluosoite: z.string().max(30),
-    postinumero: z.string().max(10),
-    postitoimipaikka: z.string().max(30),
+    jakeluosoite: z.string().max(30).optional(),
+    postinumero: z.string().max(10).optional(),
+    postitoimipaikka: z.string().max(30).optional(),
+    puhelinnumero: z.string().max(15).optional(),
     tila: z.string().max(20).refine(validateTila),
 }) satisfies z.Schema<Prisma.jasenUncheckedCreateInput>;
-
