a3be1a4e9acaa313c49b8b605bc8eb9735b41f61
eeroleppalehto <eero.leppalehto@gmail.com>
2023-11-16 15:45:55 +0200

    Zod validation for column parameters

---
+++ server/zodSchemas/columnValidation.ts
@@ -0,0 +1,15 @@
+import { z } from "zod";
+
+const columnList: string[] = ["jakoryhma.ryhma_id", "seurue_id"];
+
+const validateColumn = (column: string) => {
+    if (columnList.includes(column)) {
+        return true;
+    } else {
+        return false;
+    }
+};
+
+export const columnValidation = z
+    .string()
+    .refine(validateColumn, { message: "Invalid column parameter" });
