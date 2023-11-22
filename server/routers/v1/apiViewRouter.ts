e3ea5ba90b4efc207db10de1cd4dda15c9008dae
eeroleppalehto <eero.leppalehto@gmail.com>
2023-11-22 10:40:22 +0200

    Changed viewName to name in apiViewRouter

--- server/routers/v1/apiViewRouter.ts
+++ server/routers/v1/apiViewRouter.ts
@@ -10,18 +10,18 @@ const router = express.Router();
 
 // Define GET route for fetching view data
 router.get("/", (async (req, res) => {
-    const viewName = req.query.viewName;
+    const name = req.query.name;
 
     const { column, value } = req.query as {
         column: string | undefined;
         value: string | undefined;
     };
 
-    if (typeof viewName !== "string") {
-        throw new Error("Invalid view name");
+    if (typeof name !== "string") {
+        throw new Error("Invalid type for view name");
     }
 
-    const data = await getViewData(viewName, column, value);
+    const data = await getViewData(name, column, value);
     res.status(200).json(data); // 200 OK
 }) as express.RequestHandler);
 
