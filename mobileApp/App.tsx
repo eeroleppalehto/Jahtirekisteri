3fdfd92b27297d8872d75cdb86baa26134647c6b
eeroleppalehto <eero.leppalehto@gmail.com>
2023-12-01 10:28:49 +0200

    Added temp color palette to mobile app

--- mobileApp/App.tsx
+++ mobileApp/App.tsx
@@ -1,4 +1,4 @@
-import { PaperProvider } from "react-native-paper";
+import { PaperProvider, MD3LightTheme } from "react-native-paper";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import { NavigationContainer } from "@react-navigation/native";
 import Main from "./src/components/Main";
@@ -27,10 +27,55 @@ registerTranslation("fi", {
     close: "Sulje",
 });
 
+const theme = {
+    colors: {
+        primary: "rgb(82, 102, 0)",
+        onPrimary: "rgb(255, 255, 255)",
+        primaryContainer: "rgb(212, 237, 127)",
+        onPrimaryContainer: "rgb(23, 30, 0)",
+        secondary: "rgb(91, 97, 70)",
+        onSecondary: "rgb(255, 255, 255)",
+        secondaryContainer: "rgb(224, 230, 196)",
+        onSecondaryContainer: "rgb(25, 30, 8)",
+        tertiary: "rgb(58, 102, 94)",
+        onTertiary: "rgb(255, 255, 255)",
+        tertiaryContainer: "rgb(189, 236, 225)",
+        onTertiaryContainer: "rgb(0, 32, 27)",
+        error: "rgb(186, 26, 26)",
+        onError: "rgb(255, 255, 255)",
+        errorContainer: "rgb(255, 218, 214)",
+        onErrorContainer: "rgb(65, 0, 2)",
+        background: "rgb(254, 252, 244)",
+        onBackground: "rgb(27, 28, 23)",
+        surface: "rgb(254, 252, 244)",
+        onSurface: "rgb(27, 28, 23)",
+        surfaceVariant: "rgb(227, 228, 211)",
+        onSurfaceVariant: "rgb(70, 72, 60)",
+        outline: "rgb(119, 120, 107)",
+        outlineVariant: "rgb(199, 200, 184)",
+        shadow: "rgb(0, 0, 0)",
+        scrim: "rgb(0, 0, 0)",
+        inverseSurface: "rgb(48, 49, 43)",
+        inverseOnSurface: "rgb(243, 241, 233)",
+        inversePrimary: "rgb(184, 209, 102)",
+        elevation: {
+            level0: "transparent",
+            level1: "rgb(245, 245, 232)",
+            level2: "rgb(240, 240, 225)",
+            level3: "rgb(235, 236, 217)",
+            level4: "rgb(233, 234, 215)",
+            level5: "rgb(230, 231, 210)",
+        },
+        surfaceDisabled: "rgba(27, 28, 23, 0.12)",
+        onSurfaceDisabled: "rgba(27, 28, 23, 0.38)",
+        backdrop: "rgba(47, 49, 38, 0.4)",
+    },
+};
+
 export default function App() {
     return (
         <SafeAreaProvider>
-            <PaperProvider>
+            <PaperProvider theme={theme}>
                 <NavigationContainer>
                     <Main />
                 </NavigationContainer>
