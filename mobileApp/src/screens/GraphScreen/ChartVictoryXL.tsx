cac5ed03797aa2644ca71ba8034769abf4460162
eeroleppalehto <eero.leppalehto@gmail.com>
2023-12-01 10:56:18 +0200

    Separate table for the data on graph

--- mobileApp/src/screens/GraphScreen/ChartVictoryXL.tsx
+++ mobileApp/src/screens/GraphScreen/ChartVictoryXL.tsx
@@ -1,5 +1,17 @@
 import { View } from "react-native";
+import { ScrollView } from "react-native-gesture-handler";
+import { Text, Surface, useTheme, ActivityIndicator } from "react-native-paper";
+import { Bar, CartesianChart, useChartPressState } from "victory-native";
+import {
+    LinearGradient,
+    useFont,
+    vec,
+    Circle,
+    Text as SkText,
+} from "@shopify/react-native-skia";
+import type { SharedValue } from "react-native-reanimated";
 import useFetch from "../../../hooks/useFetch";
+
 type GroupDataChart = {
     ryhma_id: number;
     ryhman_nimi: string;
@@ -12,6 +24,16 @@ const ChartVictoryXL = () => {
     const { data, loading, error } = useFetch<GroupDataChart[]>(
         `views/?name=jakoryhma_osuus_maara`
     );
+
+    const initialYState: Record<"maara", number> = {
+        maara: 0,
+    };
+
+    const { state, isActive } = useChartPressState({
+        x: "0",
+        y: initialYState,
+    });
+
     const font = useFont(
         require("../../fonts/Roboto-Regular.ttf"),
         12,
@@ -19,6 +41,8 @@ const ChartVictoryXL = () => {
             console.log(err.message);
         }
     );
+
+    const theme = useTheme();
 
     const parsedData = data
         ? data.map((item) => {
@@ -47,31 +71,31 @@ const ChartVictoryXL = () => {
                             bottom: 400,
                         }}
                         chartPressState={state}
-                axisOptions={{
-                    font,
+                        axisOptions={{
+                            font,
                             formatXLabel(_value) {
                                 return "";
-                    },
-                }}
-            >
-                {({ points, chartBounds }) => (
-                            <>
-                    <Bar
-                                    key={"group"}
-                        chartBounds={chartBounds}
-                                    points={points.maara}
-                                    innerPadding={0.5}
-                        roundedCorners={{
-                            topLeft: 5,
-                            topRight: 5,
+                            },
                         }}
                     >
-                        <LinearGradient
-                            start={vec(0, 0)}
-                            end={vec(0, 400)}
+                        {({ points, chartBounds }) => (
+                            <>
+                                <Bar
+                                    key={"group"}
+                                    chartBounds={chartBounds}
+                                    points={points.maara}
+                                    innerPadding={0.5}
+                                    roundedCorners={{
+                                        topLeft: 5,
+                                        topRight: 5,
+                                    }}
+                                >
+                                    <LinearGradient
+                                        start={vec(0, 0)}
+                                        end={vec(0, 400)}
                                         colors={["#526600", "#52660050"]}
-                        />
-                    </Bar>
+                                    />
+                                </Bar>
                                 {isActive && (
                                     <ToolTip
                                         x={state.x.position}
@@ -84,12 +108,75 @@ const ChartVictoryXL = () => {
                                 )}
                             </>
                         )}
-            </CartesianChart>
+                    </CartesianChart>
                 )}
-        </View>
+            </View>
+            <Surface
+                elevation={1}
+                style={{
+                    marginHorizontal: 16,
+                    marginVertical: 30,
+                    borderRadius: 8,
+                    padding: 8,
+                    paddingBottom: 16,
+                }}
+            >
+                <View
+                    style={{
+                        flexDirection: "column",
+                        justifyContent: "space-between",
+                        gap: 8,
+                    }}
+                >
+                    <View
+                        style={{
+                            flexDirection: "row",
+                            justifyContent: "space-between",
+                        }}
+                    >
+                        <Text
+                            variant="titleMedium"
+                            style={{
+                                color: theme.colors.primary,
+                                marginBottom: 12,
+                            }}
+                        >
+                            Ryhmien lihat
+                        </Text>
+                        <Text
+                            variant="titleSmall"
+                            style={{
+                                color: theme.colors.outline,
+                                marginBottom: 12,
+                                marginRight: 16,
+                            }}
+                        >
+                            {"(kg)"}
+                        </Text>
+                    </View>
+                    {loading ? (
+                        <ActivityIndicator animating={true} />
+                    ) : (
+                        parsedData.map((item) => (
+                            <View
+                                key={item.group}
+                                style={{
+                                    flexDirection: "row",
+                                    justifyContent: "space-between",
+                                    marginHorizontal: 16,
+                                }}
+                            >
+                                <Text variant="bodyMedium">{item.group}</Text>
+                                <Text variant="bodyMedium">{item.maara}</Text>
+                            </View>
+                        ))
+                    )}
+                </View>
+            </Surface>
         </ScrollView>
     );
 };
+
 function ToolTip({
     x,
     y,
