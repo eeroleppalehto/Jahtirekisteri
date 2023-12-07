ae0a122ea6b5c001922da26f8fc7c6c470d0a5db
AleksanteriLohja <97512082+AleksanteriLohja@users.noreply.github.com>
2023-12-07 11:36:22 +0200

    Enhanced schema with improved validation rules

--- server/zodSchemas/jasenyysZod.ts
+++ server/zodSchemas/jasenyysZod.ts
@@ -1,25 +1,31 @@
 import { z } from "zod";
 
 const jasenyysZod = z.object({
-    // Optional integer ID for jasenyys
+    // Optional integer ID for the jasenyys entity
     jasenyys_id: z.number().int().optional(),
 
+    // Integer ID for the seurue (group) associated with this jasenyys
     seurue_id: z.number().int(),
 
-    // Integer ID for the group associated with this jasenyys
+    // Optional integer ID for the ryhma (subgroup) associated with this jasenyys
     ryhma_id: z.number().int().optional(),
 
-    // Integer ID for the member associated with this jasenyys
+    // Integer ID for the member (jasen) associated with this jasenyys
     jasen_id: z.number().int(),
 
-    // Integer representing the share of the member in the group
+    // Integer representing the member's share or participation in the group
     osuus: z.number().int(),
 
-    // DateTime for when the member joined the group
-    liittyi: z.string().datetime(),
+    // Date when the member joined the group
+    liittyi: z.date(),
 
-    // Optional DateTime for when the member left the group
-    poistui: z.string().datetime().optional(),
+    // Optional date when the member left the group
+    poistui: z.date().optional(),
 });
 
 export default jasenyysZod;
+
+// Explanation of Changes:
+// The `jasenyysZod` schema was modified to ensure consistency between the Zod schema and the Prisma schema.
+// Specifically, the types for the `liittyi` and `poistui` fields were changed from string to date types.
+// This change was necessary to match the DateTime fields in the Prisma schema, ensuring accurate data validation and handling.
