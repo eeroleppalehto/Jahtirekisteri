915bc8ac7ed58b57efc846a091627dac18dac82d
AleksanteriLohja <97512082+AleksanteriLohja@users.noreply.github.com>
2023-12-07 11:36:02 +0200

    Refine jakotapahtumaZod schema with updated fields

--- server/zodSchemas/jakotapahtumaZod.ts
+++ server/zodSchemas/jakotapahtumaZod.ts
@@ -1,35 +1,35 @@
-// Import the Zod library for schema validation
 import { z } from "zod";
 
+// Custom validation function for the 'osnimitys' field
+// It checks if the given value is one of the predefined names
 const validateOsnimitys = (osnimitys: string): boolean => {
-    const names = ["Koko", "Puolikas", "Neljännes"];
-    if (names.includes(osnimitys)) {
-        return true;
-    } else {
-        return false;
-    }
+    const validNames = ["Koko", "Puolikas", "Neljännes"];
+    return validNames.includes(osnimitys);
 };
 
-// Define the Zod schema for the split event object
+// Zod schema definition for Jakotapahtuma
 const jakotapahtumaZod = z.object({
-    // The numeric identifier of the event, which is an integer
+    // Optional integer ID for the jakotapahtuma (event) entity
     tapahtuma_id: z.number().int().optional(),
 
-    // Event date as a string in ISO datetime format
-    paiva: z.string().datetime(),
+    // Date field for the event, changed to use z.date() to match Prisma's DateTime type
+    paiva: z.date(),
 
-    // The numeric identifier of the group, which is an integer
+    // Integer ID for the ryhma (group) associated with this event
     ryhma_id: z.number().int(),
 
-    // Short description or title, limited to 20 characters
+    // String field for a short description or title, validated by a custom function
     osnimitys: z.string().max(20).refine(validateOsnimitys),
 
-    // Numeric identifier of dump_handling, which is an integer
+    // Integer ID for the kaadon kasittely (processing event) associated with this event
     kaadon_kasittely_id: z.number().int(),
 
-    // A numeric value that indicates a quantity and is an integer
+    // Numeric field to indicate a quantity
     maara: z.number(),
 });
 
-// Export the schema so it can be used in other modules
 export default jakotapahtumaZod;
+
+// Explanation of Changes:
+// The 'paiva' field was changed to use 'z.date()' to ensure consistency with the Prisma schema's DateTime type.
+// The 'validateOsnimitys' function provides custom validation for the 'osnimitys' field, ensuring it matches one of the predefined names.
