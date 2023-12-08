ec1a6dd4f2eb52a00d24b4860eee65f8dae187af
AleksanteriLohja <97512082+AleksanteriLohja@users.noreply.github.com>
2023-12-08 15:43:03 +0200

    Added comments

--- server/services/jasenService.ts
+++ server/services/jasenService.ts
@@ -1,14 +1,23 @@
-import prisma from "../client";
-import { jasenInput } from "../zodSchemas/jasenZod";
+// jasenService.ts
+import prisma from "../client"; // Import Prisma client for database operations
+import { jasenInput } from "../zodSchemas/jasenZod"; // Import Zod schema for jasen input validation
 
+/**
+ * Service module for handling CRUD operations for 'jasen' table.
+ * Utilizes Prisma ORM for database interactions and Zod for data validation.
+ */
+
+// Retrieve all 'jasen' records from the database
 const getAllJasen = async () => {
-    const jasenet = await prisma.jasen.findMany();
+    const jasenet = await prisma.jasen.findMany(); // Fetch all jasen records
     return jasenet;
 };
 
+// Create a new 'jasen' record in the database
 const createJasen = async (object: unknown) => {
-    const data = jasenInput.parse(object);
+    const data = jasenInput.parse(object); // Validate input data using Zod schema
     const { etunimi, sukunimi, jakeluosoite, postinumero, postitoimipaikka, tila } = data;
+
     const jasen = await prisma.jasen.create({
         data: {
             etunimi,
@@ -16,40 +25,41 @@ const createJasen = async (object: unknown) => {
             jakeluosoite,
             postinumero,
             postitoimipaikka,
-            tila,
+            tila
         }
     });
 
     return jasen;
 };
 
+// Update an existing 'jasen' record by its unique ID
 const updateJasen = async (id: number, object: unknown) => {
-    const data = jasenInput.parse(object);
-
+    const data = jasenInput.parse(object); // Validate and parse input data
     const { etunimi, sukunimi, jakeluosoite, postinumero, postitoimipaikka, tila } = data;
 
     const jasen = await prisma.jasen.update({
-        where: 
-            { jasen_id: id },
+        where: { jasen_id: id },
         data: {
             etunimi,
             sukunimi,
             jakeluosoite,
             postinumero,
             postitoimipaikka,
-            tila,
+            tila
         }
     });
 
     return jasen;
 };
 
+// Delete a 'jasen' record by its unique ID
 const deleteJasen = async (id: number) => {
     const jasen = await prisma.jasen.delete({
-        where: { jasen_id: id }
+        where: { jasen_id: id } // Specify the ID for deletion
     });
 
     return jasen;
 };
 
+// Export CRUD operations for external use
 export default { getAllJasen, createJasen, updateJasen, deleteJasen };
