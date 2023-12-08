f30216143b69f60e1fcbcbfb9c0b9d7bd23e43e5
AleksanteriLohja <97512082+AleksanteriLohja@users.noreply.github.com>
2023-12-08 15:39:02 +0200

    Added comments

--- server/services/jakotapahtumaService.ts
+++ server/services/jakotapahtumaService.ts
@@ -1,33 +1,41 @@
-import jakotapahtumaZod from "../zodSchemas/jakotapahtumaZod";
-// import { z } from 'zod';
-import prisma from "../client";
+// jakotapahtumaService.ts
+import jakotapahtumaZod from "../zodSchemas/jakotapahtumaZod"; // Import Zod schema for jakotapahtuma data validation
+import prisma from "../client"; // Import Prisma client for database operations
 
-// type JakotapahtumaType = z.infer<typeof jakotapahtumaZod>;
+/**
+ * Service module for handling CRUD operations for 'jakotapahtuma' table.
+ * Utilizes Prisma ORM for database interactions and Zod for input validation.
+ */
 
+// Create a new 'jakotapahtuma' record in the database
 export const createJakotapahtuma = async (data: unknown) => {
-    const parsedData = jakotapahtumaZod.parse(data);
+    const parsedData = jakotapahtumaZod.parse(data); // Validate input data using Zod schema
     return await prisma.jakotapahtuma.create({ data: parsedData });
 };
 
+// Retrieve a 'jakotapahtuma' record by its unique ID
 export const readJakotapahtumaById = async (id: number) => {
     return await prisma.jakotapahtuma.findUnique({
-        where: { tapahtuma_id: id },
+        where: { tapahtuma_id: id }, // Specify the ID to find the record
     });
 };
 
+// Update an existing 'jakotapahtuma' record by its unique ID
 export const updateJakotapahtumaById = async (id: number, data: unknown) => {
-    console.log("data: ", data);
-    const parsedData = jakotapahtumaZod.parse(data);
+    console.log("data: ", data); // Optional: Logging the input data
+    const parsedData = jakotapahtumaZod.parse(data); // Validate and parse input data
     return await prisma.jakotapahtuma.update({
         where: { tapahtuma_id: id },
         data: parsedData,
     });
 };
 
+// Delete a 'jakotapahtuma' record by its unique ID
 export const deleteJakotapahtumaById = async (id: number) => {
-    return await prisma.jakotapahtuma.delete({ where: { tapahtuma_id: id } });
+    return await prisma.jakotapahtuma.delete({ where: { tapahtuma_id: id } }); // Specify the ID for deletion
 };
 
+// Retrieve all 'jakotapahtuma' records from the database
 export const getAllJakotapahtumat = async () => {
-    return await prisma.jakotapahtuma.findMany();
+    return await prisma.jakotapahtuma.findMany(); // Fetch all jakotapahtuma records
 };
