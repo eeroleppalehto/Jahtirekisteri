4923fb8b74e342ba34527ae2f57cb465dbd8bf15
AleksanteriLohja <97512082+AleksanteriLohja@users.noreply.github.com>
2023-12-08 15:47:10 +0200

    Added comments

--- server/services/kaadonkasittelyService.ts
+++ server/services/kaadonkasittelyService.ts
@@ -1,35 +1,42 @@
-import kaadonkasittelySchema from "../zodSchemas/kaadonkasittelyZod";
-// import { z } from "zod";
-import prisma from "../client";
+// kaadonkasittelyService.ts
+import kaadonkasittelySchema from "../zodSchemas/kaadonkasittelyZod"; // Import Zod schema for kaadonkasittely data validation
+import prisma from "../client"; // Import Prisma client for database operations
 
-// type KaadonkasittelyType = z.infer<typeof kaadonkasittelySchema>;
+/**
+ * Service module for handling CRUD operations for 'kaadon_kasittely' table.
+ * Utilizes Prisma ORM for database interactions and Zod for input validation.
+ */
 
+// Create a new 'kaadon_kasittely' record in the database
 export const createKaadonkasittely = async (data: unknown) => {
-    const parsedData = kaadonkasittelySchema.parse(data);
+    const parsedData = kaadonkasittelySchema.parse(data); // Validate input data using Zod schema
     return await prisma.kaadon_kasittely.create({ data: parsedData });
 };
 
+// Retrieve a 'kaadon_kasittely' record by its unique ID
 export const readKaadonkasittelyById = async (id: number) => {
     return await prisma.kaadon_kasittely.findUnique({
         where: { kaadon_kasittely_id: id },
     });
 };
 
+// Update an existing 'kaadon_kasittely' record by its unique ID
 export const updateKaadonkasittelyById = async (id: number, data: unknown) => {
-    const parsedData = kaadonkasittelySchema.parse(data);
-
+    const parsedData = kaadonkasittelySchema.parse(data); // Validate and parse input data
     return await prisma.kaadon_kasittely.update({
         where: { kaadon_kasittely_id: id },
         data: parsedData,
     });
 };
 
+// Delete a 'kaadon_kasittely' record by its unique ID
 export const deleteKaadonkasittelyById = async (id: number) => {
     return await prisma.kaadon_kasittely.delete({
         where: { kaadon_kasittely_id: id },
     });
 };
 
+// Retrieve all 'kaadon_kasittely' records from the database
 export const getAllKaadonkasittelyt = async () => {
     return await prisma.kaadon_kasittely.findMany();
 };
