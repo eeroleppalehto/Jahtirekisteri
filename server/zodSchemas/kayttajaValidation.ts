65401dbe39d245e2557829da7b732d128287d982
eeroleppalehto <eero.leppalehto@gmail.com>
2024-01-17 14:03:35 +0200

    Zod validation for user

---
+++ server/zodSchemas/kayttajaValidation.ts
@@ -0,0 +1,9 @@
+import { Prisma } from "@prisma/client";
+import { z } from "zod";
+
+export const kayttajaInput = z.object({
+    kayttaja_id: z.number().int().positive(),
+    kayttajatunnus: z.string().max(32),
+    salasana_hash: z.string().max(255),
+    sahkoposti: z.string().email().max(64).optional(),
+}) satisfies z.Schema<Prisma.kayttajaUncheckedCreateInput>;
