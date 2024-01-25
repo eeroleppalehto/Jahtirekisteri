7f1fbe872bc73eb292bc4319a63bfbf294f390fa
eeroleppalehto <eero.leppalehto@gmail.com>
2024-01-25 15:55:34 +0200

    misc

--- server/routers/v2/testRouter.ts
+++ server/routers/v2/testRouter.ts
@@ -1,33 +1,26 @@
 import express from "express";
-import jwt from "jsonwebtoken";
 import "dotenv/config";
+import {
+    getDecodedToken,
+    READ_RIGHTS_SET,
+} from "../../utils/authenticationUtils";
 import kayttajaService from "../../services/kayttajaService";
 
 const testRouter = express.Router();
 
-const getDecodedToken = (req: express.Request) => {
-    const token = req.get("authorization");
-
-    if (!token) throw new Error("Token missing");
-
-    if (!token.startsWith("Bearer ")) throw new Error("Invalid token");
-
-    const filteredToken = token.replace("Bearer ", "");
-
-    return jwt.verify(filteredToken, process.env.JWT_SECRET as string) as {
-        kayttaja_id: number;
-        kayttajatunnus: string;
-    };
-};
-
 testRouter.get("/", (async (req, res) => {
     const decodedToken = getDecodedToken(req);
 
+    if (!READ_RIGHTS_SET.has(decodedToken.rooli)) {
+        return res.status(401).json({ error: "Unauthorized" });
+    }
+
+    console.log(decodedToken);
     const jasen = await kayttajaService.getKayttajaByUsername(
         decodedToken.kayttajatunnus
     );
 
-    return res.status(200).json({ message: "Valid token", data: jasen });
+    return res.status(200).json({ message: "Valid token", jasen });
 }) as express.RequestHandler);
 
 export default testRouter;
