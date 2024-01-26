5f1fb409245ce8f556f5711e5697cf80851d1b56
eeroleppalehto <eero.leppalehto@gmail.com>
2024-01-26 14:25:45 +0200

    Updated MemberScreen to fetch data from different endpoint

--- mobileApp/src/screens/MemberScreen/index.tsx
+++ mobileApp/src/screens/MemberScreen/index.tsx
@@ -1,11 +1,16 @@
-import { ActivityIndicator, Text, useTheme } from "react-native-paper";
+import { Text, useTheme } from "react-native-paper";
 import { useState } from "react";
-import { FlatList, RefreshControl } from "react-native-gesture-handler";
-import { Jasen } from "../../types";
+import { RefreshControl } from "react-native-gesture-handler";
+import { JasenStateQuery } from "../../types";
 import MemberListItem from "./MemberListItem";
 import { MaintenanceTabScreenProps } from "../../NavigationTypes";
 import FloatingNavigationButton from "../../components/FloatingNavigationButton";
-import { NativeScrollEvent, NativeSyntheticEvent, View } from "react-native";
+import {
+    NativeScrollEvent,
+    NativeSyntheticEvent,
+    View,
+    SectionList,
+} from "react-native";
 import useFetch from "../../hooks/useFetch";
 
 type Props = MaintenanceTabScreenProps<"Jäsenet">;
@@ -13,12 +18,14 @@ type Props = MaintenanceTabScreenProps<"Jäsenet">;
 
 type MembersByLetter = {
     letter: string;
-    members: Jasen[];
+    members: JasenStateQuery[];
 };
 
 // Screen for displaying all members in Maintenance tab
 function MemberScreen({ navigation, route }: Props) {
-    const { data, loading, error, onRefresh } = useFetch<Jasen[]>("members");
+    const { data, loading, error, onRefresh } = useFetch<JasenStateQuery[]>(
+        "views/?name=jasen_tila_indeksilla"
+    );
     const [scrollValue, setScrollValue] = useState(0);
 
     const theme = useTheme();
@@ -47,20 +54,33 @@ function MemberScreen({ navigation, route }: Props) {
     });
 
     // Group members by first letter of last name
-    const membersByLetter = sortedMembers?.reduce((acc, member) => {
+    // const membersByLetter = sortedMembers?.reduce((acc, member) => {
+    //     const firstLetter = member.sukunimi.charAt(0).toUpperCase();
+    //     if (!acc.some((item) => item.letter === firstLetter)) {
+    //         acc.push({
+    //             letter: firstLetter,
+    //             members: [member],
+    //         });
+    //     } else {
+    //         acc.find((item) => item.letter === firstLetter)?.members.push(
+    //             member
+    //         );
+    //     }
+    //     return acc;
+    // }, [] as { letter: string; members: JasenStateQuery[] }[]);
+
+    const membersByTitle = sortedMembers?.reduce((acc, member) => {
         const firstLetter = member.sukunimi.charAt(0).toUpperCase();
-        if (!acc.some((item) => item.letter === firstLetter)) {
+        if (!acc.some((item) => item.title === firstLetter)) {
             acc.push({
-                letter: firstLetter,
-                members: [member],
+                title: firstLetter,
+                data: [member],
             });
         } else {
-            acc.find((item) => item.letter === firstLetter)?.members.push(
-                member
-            );
+            acc.find((item) => item.title === firstLetter)?.data.push(member);
         }
         return acc;
-    }, [] as { letter: string; members: Jasen[] }[]);
+    }, [] as { title: string; data: JasenStateQuery[] }[]);
 
     const MemberContent = ({ letter, members }: MembersByLetter) => {
         return (
@@ -92,23 +112,38 @@ function MemberScreen({ navigation, route }: Props) {
 
     return (
         <>
-            <FlatList //TODO: Add sticky headers? https://reactnative.dev/docs/flatlist#stickyheadersenabled
-                data={membersByLetter}
-                keyExtractor={(item) => item.letter}
-                refreshControl={
-                    <RefreshControl
-                        refreshing={loading}
-                        onRefresh={onRefresh}
-                    />
-                }
-                renderItem={({ item }) => (
-                    <MemberContent
-                        letter={item.letter}
-                        members={item.members}
-                    />
-                )}
-                onScroll={onScroll}
-            />
+            {membersByTitle && (
+                <SectionList
+                    sections={membersByTitle}
+                    keyExtractor={(item) => item.jasen_id.toString()}
+                    renderItem={({ item }) => (
+                        <MemberListItem
+                            jasen={item}
+                            navigation={navigation}
+                            key={item.jasen_id}
+                        />
+                    )}
+                    renderSectionHeader={({ section: { title } }) => (
+                        <Text
+                            variant="titleMedium"
+                            style={{
+                                color: theme.colors.primary,
+                                paddingLeft: 16,
+                                paddingTop: 15,
+                            }}
+                        >
+                            {title}
+                        </Text>
+                    )}
+                    stickySectionHeadersEnabled={true}
+                    refreshControl={
+                        <RefreshControl
+                            refreshing={loading}
+                            onRefresh={onRefresh}
+                        />
+                    }
+                />
+            )}
             <FloatingNavigationButton
                 scrollValue={scrollValue}
                 type="jäsen"
