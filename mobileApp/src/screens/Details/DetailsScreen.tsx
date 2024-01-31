f51e93f0c155b798014257bc01e992324ccf95eb
eeroleppalehto <eero.leppalehto@gmail.com>
2024-01-31 12:26:24 +0200

    Navigation for party details

--- mobileApp/src/screens/Details/DetailsScreen.tsx
+++ mobileApp/src/screens/Details/DetailsScreen.tsx
@@ -1,7 +1,8 @@
 import MemberDetails from "./MemberDetails";
 import ShotDetails from "./ShotDetails";
 import GroupDetails from "./GroupDetails";
-import ErrorScreen from "../ErrorScreen";
+import { PartyDetails } from "./PartyDetails";
+import { ErrorScreen } from "../ErrorScreen";
 import { RootStackScreenProps } from "../../NavigationTypes";
 
 type Props = RootStackScreenProps<"Details">;
@@ -9,7 +10,10 @@ type Props = RootStackScreenProps<"Details">;
 // Screen for displaying details screens for different types
 // if type is not found, displays an error screen
 function DetailsScreen({ route, navigation }: Props) {
-    if (!route.params?.type) return <ErrorScreen />;
+    if (!route.params?.type)
+        return (
+            <ErrorScreen error={new Error("Tapahtui virhe navigaatiossa")} />
+        );
     try {
         // Get type from route params
         const { type } = route.params;
@@ -22,13 +26,21 @@ function DetailsScreen({ route, navigation }: Props) {
                 return <ShotDetails route={route} navigation={navigation} />;
             case "Ryhmä":
                 return <GroupDetails route={route} navigation={navigation} />;
+            case "Seurue":
+                return <PartyDetails route={route} navigation={navigation} />;
             default:
-                return <ErrorScreen />;
+                return (
+                    <ErrorScreen
+                        error={new Error("Tapahtui virhe navigaatiossa")}
+                    />
+                );
         }
     } catch (error) {
         console.log("Screen type not found");
         if (error instanceof Error) console.log(error.message);
-        return <ErrorScreen />;
+        return (
+            <ErrorScreen error={new Error("Tapahtui virhe navigaatiossa")} />
+        );
     }
 }
 
