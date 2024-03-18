ddb9cc3714f028b1befc1165e4657641582cb4fc
eeroleppalehto <eero.leppalehto@gmail.com>
2024-03-18 15:42:06 +0200

    API endpoint for group share chart

--- server/routers/v2/graphRouter.ts
+++ server/routers/v2/graphRouter.ts
@@ -27,4 +27,34 @@ graphRouter.get("/deer-count", (async (req, res) => {
     res.status(200).json(data); // 200 OK
 }) as express.RequestHandler);
 
+graphRouter.get("/group-shares", (async (req, res) => {
+    const decodedToken = getDecodedToken(req);
+
+    if (!READ_RIGHTS_SET.has(decodedToken.rooli)) {
+        return res.status(401).json({ error: "Unauthorized" });
+    }
+
+    const groupShareData = await prisma.$queryRaw`
+        SELECT jakoryhma.ryhma_id,
+            jakoryhma.ryhman_nimi,
+            jakoryhma.seurue_id,
+            ryhmien_osuudet.jakokerroin AS osuus,
+            sum(jakotapahtuma.maara) AS maara
+        FROM jakoryhma
+            LEFT JOIN jakotapahtuma ON jakoryhma.ryhma_id = jakotapahtuma.ryhma_id
+            LEFT JOIN ryhmien_osuudet ON jakoryhma.ryhma_id = ryhmien_osuudet.ryhma_id
+            JOIN seurue ON jakoryhma.seurue_id = seurue.seurue_id
+        WHERE seurue.seurue_tyyppi_id = 1
+        GROUP BY jakoryhma.ryhma_id, jakoryhma.ryhman_nimi, jakoryhma.seurue_id, ryhmien_osuudet.jakokerroin
+        ORDER BY ryhman_nimi;`;
+
+    if (!groupShareData) {
+        return res.status(404).json({ error: "Not found" });
+    }
+
+    // const groupMeatSum = await prisma.$queryRaw`
+
+    res.status(200).json(groupShareData); // 200 OK
+}) as express.RequestHandler);
+
 export default graphRouter;
